d757f626542d497ea680f4d3b3256047632ee5d2
Isaac Kleisle-Murphy <IKleisle@DN0a1e8cee.SUNet>
2022-02-21 23:49:17 -0800

    upload hw3

---
+++ cs234/hw3/code/run_cartpole.sh
@@ -0,0 +1,8 @@
+python main.py --env-name "cartpole" --seed 2020 --no-baseline;
+python main.py --env-name "cartpole" --seed 2020 --baseline;
+python main.py --env-name "cartpole" --seed 2021 --no-baseline;
+python main.py --env-name "cartpole" --seed 2021 --baseline;
+python main.py --env-name "cartpole" --seed 2022 --no-baseline;
+python main.py --env-name "cartpole" --seed 2022 --baseline;
+
+python main.py --env-name "cartpole" --seed 25 --no-baseline;
---
+++ cs234/hw3/code/run_cheetah.sh
@@ -0,0 +1,6 @@
+python main.py --env-name "cheetah" --seed 20 --no-baseline;
+python main.py --env-name "cheetah" --seed 20 --baseline;
+python main.py --env-name "cheetah" --seed 21 --no-baseline;
+python main.py --env-name "cheetah" --seed 21 --baseline;
+python main.py --env-name "cheetah" --seed 22 --no-baseline;
+python main.py --env-name "cheetah" --seed 22 --baseline;
